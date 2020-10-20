e194b11a7facda342bbb863bcbaafc1b640c5e4f
zhaoj154 <jiayu.zhao@mail.utoronto.ca>
2020-10-19 23:52:51 -0400

    Updates

---
+++ precache-manifest.a44c98615293b2d94a6db1de372195ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b90cc9a8ead50ee61924e4908706027",
+    "url": "/Jay-Todo-List/index.html"
+  },
+  {
+    "revision": "3d7b027a5638bdf8434f",
+    "url": "/Jay-Todo-List/static/css/main.3706e840.chunk.css"
+  },
+  {
+    "revision": "d124a18f49ce8dca62b8",
+    "url": "/Jay-Todo-List/static/js/2.3859e4cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Jay-Todo-List/static/js/2.3859e4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d7b027a5638bdf8434f",
+    "url": "/Jay-Todo-List/static/js/main.2e201e25.chunk.js"
+  },
+  {
+    "revision": "628bc2e48fe9460a972c",
+    "url": "/Jay-Todo-List/static/js/runtime-main.f5a9ae0e.js"
+  }
+]);
